9b40f4a31c0b562347803428f9e8e66d36d6069b
raydeng83 <ray.deng83@gmail.com>
2018-10-07 17:04:08 -0400

    add support for chinese in table

--- btm-fe/src/app/app-const.ts
+++ btm-fe/src/app/app-const.ts
@@ -1,3 +1,5 @@
 export class AppConst {
-    public static serverPath= 'http://149.28.61.74:8181/btm-be';
+    // public static serverPath= 'http://149.28.61.74:8181/btm-be';
+    public static serverPath= 'http://localhost:8181/btm-be';
+
 }
